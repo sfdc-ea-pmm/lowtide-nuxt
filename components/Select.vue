48af4caea1d133c884a51bb9b530238f009618e6
romariosc97 <romarito_97@hotmail.com>
2021-04-23 12:36:03 -0500

    Cancel button in Steps: Fixed

--- components/Select.vue
+++ components/Select.vue
@@ -6,7 +6,7 @@
         <div class="relative">
             <button :disabled="this.step>0" @click="changeStatus()" type="button" :class="(this.step>0 ? 'cursor-not-allowed ' : '' ) + 'disabled:opacity-50 bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2.5 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label">
                 <span class="block truncate">
-                    {{selected}}
+                    {{ action==='Welcome!' && currentStep===0 ? this.label : selected}}
                 </span>
                 <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                     <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -43,7 +43,12 @@ export default {
         }
     },
     computed: {
-
+        action () {
+            return this.$store.state.action;
+        },
+        currentStep () {
+            return this.$store.state.currentStep;
+        }
     },
 
     data() {
