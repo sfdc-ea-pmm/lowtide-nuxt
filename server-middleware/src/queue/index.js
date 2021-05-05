69a2478890d0f66b09692c22e8ac5b7fd4819bc7
romariosc97 <romarito_97@hotmail.com>
2021-05-05 18:01:32 -0500

    .

--- server-middleware/src/queue/index.js
+++ server-middleware/src/queue/index.js
@@ -1,94 +1,51 @@
 const Queue = require('bull')
-
 const { runDataflow, generateTimeshiftDataflow, deleteDataflow } = require('../dataflow'),
       refreshDatasets = require('../refresh'),
       deployTemplate = require('../deploy');
-
 const restQueue = new Queue('restQueue', process.env.REDIS_URL)
 const deployQueue = new Queue('deployQueue', process.env.REDIS_URL)
 const timeshiftQueue = new Queue('timeshiftQueue', process.env.REDIS_URL)
-
-jobQueue.process('template_deploy', (job) => {
-  const params = job.data
-  const conn = auth.refresh(params.session)
-  return s3.downloadAndDeployTemplate(conn, params)
+/* Processes */
+deployQueue.process('template_deploy', async (job) => {
+  const { session, template } = job.data
+  const jobExecResult = await deployTemplate({ session, job, template })
+  return jobExecResult
 })
-
-jobQueue.process('generate_timeshift_dataflow', (job) => {
-  const params = job.data
-  const conn = auth.refreshConnection(params.session)
-  return ts.generateTimeshiftDataflow(conn, params)
+/* Timeshift 4 step process; refresh -> generate -> run dataflow -> delete primer */
+timeshiftQueue.process('refresh_stale_datasets', async (job) => {
+  const jobExecResult = await refreshDatasets(job)
+  return jobExecResult
 })
-
-const sendUpdate = (job, message, object) => {
-  console.log('Send Socket Update.')
-  // io.to(job.data.session.socketRoom).emit('jobUpdate', {
-  //   message: message, ...object
-  // })
-}
-
-// jobQueue.on("completed", (job, result) => {
-//   console.log(`Job with ID ${job.id} has been completed.`)
-//   io.to(job.data.session.socketRoom).emit('jobEnded', {
-//     message: 'Job has completed.',
-//     id: job.id,
-//     result: result,
-//     template_keys: job.data.template_keys || null,
-//     data: job.data
-//   })
-// })
-//
-// jobQueue.on("failed", (job, err) => {
-//   console.error(`Job with ID ${job.id} has failed.`)
-//   sendUpdate(job, 'Job has failed.', { job, err })
-// })
-//
-// jobQueue.on("error", (error) => {
-//   console.error(`A job error occurred: ${error.message}.`)
-// })
-//
-// jobQueue.on("waiting", (jobId) => {
-//   console.log(`Job with ID ${jobId} is waiting.`)
-// });
-//
-// jobQueue.on("active", (job, jobPromise) => {
-//   console.error(`Job with ID ${job.id} has started.`)
-//   sendUpdate(job, 'Job has started.', { job })
-// })
-//
-// jobQueue.on("stalled", (job) => {
-//   console.warning(`Job with ID ${job.id} has stalled.`)
-//   sendUpdate(job, 'Job has stalled.', { job })
-// })
-//
-// jobQueue.on("progress", (job, progress) => {
-//   console.log(`Job with ID ${job.id} has progressed.`)
-//   sendUpdate(job, 'Job has progressed.', { job, progress })
-// })
-//
-// jobQueue.on("drained", () => {
-//   console.log('Job queue has drained.')
-// })
-
+timeshiftQueue.process('generate_timeshift_dataflow', async (job) => {
+  const jobExecResult = await generateTimeshiftDataflow(job)
+  return jobExecResult
+})
+timeshiftQueue.process('delete_primer_dataflow', async (job) => {
+  const { dataflowId } = job.data
+  const jobExecResult = await deleteDataflow({ job, dataflowId })
+  return jobExecResult
+})
+restQueue.process('dataflow_run', async (job) => {
+  const { session, params, body } = job.data
+  const jobExecResult = await runDataflow({ session, job, dataflowId: params.id })
+  return jobExecResult
+})
+/* Helper to standardize format and exclude some attributes. */
+const formatJobResponse = function(job) {
+  const { id, name, opts, data, ...rest } = job,
+        { params, body } = data,
+        dateKeys = ['timestamp', 'finishedOn', 'processedOn'];
   delete rest.queue
   delete rest.stacktrace
-
   const response = { id, name, params, body, ...rest }
-
   for (const [key, value] of Object.entries(response))
     if (dateKeys.includes(key))
       response[key] = new Date(value)
-
   return response
-
 }
-
 /* Events */
-
 const setListeners = function(emitter) {
-
   const logEvents = [ 'paused', 'drained', 'cleaned' ];
-
   const coerceSendEvents = {
     active: 'jobStarted',
     stalled: 'jobInfo',
@@ -96,10 +53,8 @@ const setListeners = function(emitter) {
     failed: 'jobError',
     completed: 'jobSuccess'
   }
-
   for (const e of logEvents)
     emitter.on(e, () => console.log(`Internal: lowtide.${emitter.name} event: ${e}.`))
-
   for (const [key, evt] of Object.entries(coerceSendEvents)) {
     if (['progress', 'failed', 'completed'].includes(key))
       emitter.on(key, (job, data) => {
@@ -116,11 +71,8 @@ const setListeners = function(emitter) {
         })
       })
   }
-
   return emitter
-
 }
-
 module.exports = {
   restQueue: setListeners(restQueue),
   deployQueue: setListeners(deployQueue),
