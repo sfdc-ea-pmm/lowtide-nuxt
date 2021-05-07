1209e8d2f43e2abff02a3567a3a342bf8d65fcbe
luciyer <luciyer@gmail.com>
2021-05-06 17:46:07 -0700

    remove sm-backup

--- sm-backup/src/router/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  timeshift: require("./timeshift"),
-  jobs: require("./jobs"),
-  org: require("./org"),
-  repo: require("./repo")
-}
--- sm-backup/src/timeshift/classes/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  BatchQuery: require("./BatchQuery"),
-  Dataflow: require("./Dataflow"),
-  LatestDateQuery: require("./LatestDateQuery"),
-  RefreshQuery: require("./RefreshQuery")
-}
