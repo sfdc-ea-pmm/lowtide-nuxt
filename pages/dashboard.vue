ab290af18b34abd86217800d3eeeec39f5ec0e05
luciyer <luciyer@gmail.com>
2021-05-15 09:05:10 -0700

    remove API_URL reference from axios call

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -158,7 +158,7 @@ export default {
         async logout() {
             try {
                 this.logoutLoading = true;
-                await this.$axios.get(`${process.env.API_URL}/auth/revoke`, {withCredentials: true});
+                await this.$axios.get(`/auth/revoke`, {withCredentials: true});
                 this.$store.commit(`setNotifications` , []);
                 sessionStorage.removeItem('notifications');
                 this.finished();
@@ -219,9 +219,12 @@ export default {
 
         activeEvents.forEach(event => {
             vm.socket.on(event, (message) => {
-                console.log(message, event);
+
+                console.log('Socket:', message, event);
+
                 let currentTime = this.getCurrentTime();
                 let type, text;
+
                 switch (event) {
                     case 'jobError' || 'serverError':
                         type = 'error';
@@ -281,14 +284,7 @@ export default {
                 }
             });
         });
-        /*
-        const eventsList = [ 'jobStarted', 'jobInfo', 'jobSuccess', 'jobError', 'serverError' ];
-        for (const e of eventsList) {
-            vm.socket.on(e, (message) => {
-                console.log(message);
-            });
-        }
-        */
+
     },
     watch: {
         action: function () {
