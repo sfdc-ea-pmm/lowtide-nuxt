6144428cea799adb0a0c6a6737ec4a10caa163af
luciyer <luciyer@gmail.com>
2021-05-20 12:49:17 -0700

    formatting, readme

--- components/Toast.vue
+++ components/Toast.vue
@@ -6,7 +6,6 @@
                     <div class="p-4">
                         <div class="flex items-start">
                             <div class="flex-shrink-0 self-center">
-                                <!-- Heroicon name: outline/check-circle -->
                                 <svg v-show="v.type==='success'" class="h-7 w-7 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
@@ -44,34 +43,25 @@
 
 <script>
 export default {
-    computed: {
-        toastStatus () {
-            return this.$store.state.toastStatus;
-        }
+  computed: {
+    toastStatus () {
+      return this.$store.state.toastStatus;
+    }
+  },
+  methods: {
+    toastClose(index){
+      this.$store.commit(`setToastStatus` , this.toastStatus.filter((v, i) => i===index ? false : true));
     },
-
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-        toastClose(index){
-            this.$store.commit(`setToastStatus` , this.toastStatus.filter((v, i) => i===index ? false : true));
-        },
-    },
-    created() {
-        
-    },
+  }
 }
 </script>
 
 <style>
-    .toast-enter-active, .toast-leave-active{
-        transition: all 0.5s;
-    }
-    .toast-enter, .toast-leave-to {
-        opacity: 0;
-        transform: translateX(30px);
-    }
+.toast-enter-active, .toast-leave-active{
+  transition: all 0.5s;
+}
+.toast-enter, .toast-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
 </style>
