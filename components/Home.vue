ca81f8fdf8e0b0c18b7a6977079a8801520b5971
luciyer <luciyer@gmail.com>
2021-05-20 10:58:23 -0700

    video link

--- components/Home.vue
+++ components/Home.vue
@@ -13,7 +13,7 @@
                 These tools are currently in <strong>alpha testing</strong> stage, so expect that occasionally bugs or oddities may appear. Please bear with us as we improve them and continuously add new functionality and net new tools.
                 </p>
                 <p>
-                We hope that this application is relatively self-explanatory, but in the interest of being as user-friendly as possible, please <a class="font-bold text-blue-600 cursor-pointer hover:text-blue-500" href="#">start by watching this video tutorial</a>, and <a class="font-bold text-blue-600 cursor-pointer hover:text-blue-500" href="#" @click.prevent="renderFAQ()">read the FAQ</a>.
+                We hope that this application is relatively self-explanatory, but in the interest of being as user-friendly as possible, please <a class="font-bold text-blue-600 cursor-pointer hover:text-blue-500" target="_blank" href="https://www.loom.com/share/299e1a7a27aa4b1b872ab8cdf9c44946">start by watching this video tutorial</a>, and <a class="font-bold text-blue-600 cursor-pointer hover:text-blue-500" href="#" @click.prevent="renderFAQ()">read the FAQ</a>.
                 </p>
                 <p>
                 For developers or those with enough knowledge of javascript to be dangerous, we've made available <a class="font-bold text-blue-600 cursor-pointer hover:text-blue-500" target="_blank" href="https://github.com/romariosc97/lowtide-boilerplate">lowtide-boilerplate</a>, an application shell designed to allow you to develop your own tools by providing a simple API layer with authentication into Salesforce and the basic frontend skeleton that you see in this very app.
